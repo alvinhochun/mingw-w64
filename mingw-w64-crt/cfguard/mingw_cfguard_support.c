3cb9552e2abc8e6dba54c77a1a89702dfc6116bd
Alvin Wong <alvinhochun@gmail.com>
2022-08-26 21:30:19 +0800

    Clean up mingw_cfguard_support.c a bit

--- mingw-w64-crt/cfguard/mingw_cfguard_support.c
+++ mingw-w64-crt/cfguard/mingw_cfguard_support.c
@@ -22,25 +22,26 @@ extern void *__guard_dispatch_icall_dummy;
 
 #elif defined(__i386__) || defined(__aarch64__) || defined(__arm__)
 
-// FIXME: Check correctness.
-
 static void __guard_check_icall_dummy() {
     // The target address is passed via %ecx (x86), X15 (aarch64) or R0 (arm).
-}
-
-static void __guard_dispatch_icall_dummy() {
-    // This is not used outside of x86_64.
-    __builtin_trap();
 }
 
 #else
 #   error "CFGuard support is unimplemented for the current architecture."
 #endif
 
+// I am not sure about the `.00cfg` section. This is just an attempt to follow
+// what VC runtime defines. The MSVC linker appears to merge this section into
+// `.rdata`, but LLD does not do this at the time of writing.
+
 __asm__(".section .00cfg,\"dr\"");
 
-__attribute (( section (".00cfg") ))
-volatile void *__guard_check_icall_fptr = &__guard_check_icall_dummy;
+__attribute__(( section (".00cfg") ))
+void *__guard_check_icall_fptr = &__guard_check_icall_dummy;
 
-__attribute (( section (".00cfg") ))
-volatile void *__guard_dispatch_icall_fptr = &__guard_dispatch_icall_dummy;
+#if defined(__x86_64__)
+
+__attribute__(( section (".00cfg") ))
+void *__guard_dispatch_icall_fptr = &__guard_dispatch_icall_dummy;
+
+#endif
